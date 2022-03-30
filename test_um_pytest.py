5bb59fd9e41264e04e8740dfc57b0a54d81eecbf
STIRBUA427 <alexandru.stirbu@fisglobal.com>
2022-03-30 23:21:16 +0300

    added new function tu unnecessary_math tests

--- test_um_pytest.py
+++ test_um_pytest.py
@@ -2,6 +2,9 @@ from unnecessary_math import multiply
 
 def test_numbers_3_4():
   assert multiply(3,4) == 12
-  
+
 def test_strings_a_3():
   assert multiply('a',3) == 'aaa'
+
+def test_numbers_5_4():
+    assert multiply(5,4) != 21
